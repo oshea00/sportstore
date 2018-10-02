3f6b6104452022f3b6e8fdf2e5bf90c13858ab8c
Mike OShea <oshea00@gmail.com>
2018-10-02 15:41:35 -0700

    init

---
+++ SportsStore/Models/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SportsStore.Models {
+
+    public class ApplicationDbContext : DbContext {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured) {
+                optionsBuilder.UseInMemoryDatabase("product");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    public class ApplicationDbContextFactory
+            : IDesignTimeDbContextFactory<ApplicationDbContext> {
+
+        public ApplicationDbContext CreateDbContext(string[] args) =>
+            Program.BuildWebHost(args).Services
+                .GetRequiredService<ApplicationDbContext>();
+    }
+}
---
+++ SportsStore/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+namespace SportsStore.Models {
+
+    [Table("products")]
+    public class Product {
+        [Key]
+        [Column("productid")]
+        public int ProductID { get; set; }
+        [Column("name")]
+        public string Name { get; set; }
+        [Column("description")]
+        public string Description { get; set; }
+        [Column("price")]
+        public decimal Price { get; set; }
+        [Column("category")]
+        public string Category { get; set; }
+    }
+}
+
